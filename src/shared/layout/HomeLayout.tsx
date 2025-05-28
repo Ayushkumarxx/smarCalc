7fca9e767142244b3ef243a1243161c0929720ad
ayush kumar <ayushkumar09005@gmail.com>
2025-05-28 17:33:58 +0530

    first commit

--- src/shared/layout/HomeLayout.tsx
+++ src/shared/layout/HomeLayout.tsx
@@ -8,17 +8,24 @@ const HomeLayout: React.FC<any> = ({
   return (
     <div className="min-h-screen w-full relative overflow-x-hidden bg-theme-background">
       {/* Fixed Rotated Background */}
-      <div
-        className="fixed inset-0 w-full h-[1000px] transform rotate-12 scale-150 bg-theme-background"
-        style={{
-          backgroundImage: `
-        linear-gradient(#918D82 .5px, transparent .5px), 
-        linear-gradient(to right, #918D82 .5px, transparent .5px)
-      `,
-          backgroundSize: "250px 150px",
-     
-        }}
-      />
+   <div
+  className="
+    fixed inset-0 w-full 
+    h-[600px] sm:h-[800px] md:h-[1000px] 
+    transform 
+    rotate-6 sm:rotate-12 
+    scale-125 sm:scale-150 
+    bg-theme-background
+  "
+  style={{
+    backgroundImage: `
+      linear-gradient(#918D82 .5px, transparent .5px), 
+      linear-gradient(to right, #918D82 .5px, transparent .5px)
+    `,
+    backgroundSize: "250px 150px",
+  }}
+/>
+
       
 
       {/* Scrollable Content */}
